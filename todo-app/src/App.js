ba8b507526e70885fd75bfece922a33eb786c830
Nareshsirvi <nareshsirvi842@gmail.com>
2024-08-09 16:53:52 +0530

    first commit

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TodoForm from './Components/TodoForm';
+import TodoWrapper from './Components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+     <TodoWrapper />
+  
+    </div>
+  );
+}
+
+export default App;
